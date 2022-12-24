6051b72b56c8fd39fc0a37fa78e7ce235f2fc27c
reshmampl2018 <reshmampl2018@gmai.com>
2022-12-24 18:18:30 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+from app.forms import *
+
+def insert_topic(request):
+    form=TopicForm()
+    d={'form':form}
+    if request.method=='POST':
+        form_data=TopicForm(request.POST)
+        if form_data.is_valid():
+            form_data.save()
+            return HttpResponse('model is saves successfully')
+    return render(request,'insert_topic.html',d)
+
+def insert_webpage(request):
+    form=WebpageForm()
+    d={'form':form}
+    if request.method=='POST':
+        form_data=WebpageForm(request.POST)
+        if form_data.is_valid():
+            form_data.save()
+            return HttpResponse('webpage model is saves successfully')
+    return render(request,'insert_webpage.html',d)
+
+def insert_accessrecords(request):
+    form=AccessRecordsForm()
+    d={'form':form}
+    if request.method=='POST':
+        form_data=AccessRecordsForm(request.POST)
+        if form_data.is_valid():
+            form_data.save()
+            return HttpResponse('accessrecords model is saves successfully')
+    return render(request,'insert_accessrecords.html',d)
+
+        
